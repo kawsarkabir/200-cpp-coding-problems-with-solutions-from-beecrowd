030602dc1706bbd0222e51359e6cb78ee485c981
kawsarkabir <devkawsarkabir@gmail.com>
2024-03-12 00:49:31 +0600

    Hello World and Extreamly Basic Problem solve

---
+++ 1001 - Extremely Basic/Extremely_Basic.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    int A, B;
+
+    // Read inputs A and B
+    cin >> A >> B;
+
+    // Calculate the sum
+    int X = A + B;
+
+    // Print the result
+    cout << "X = " << X << endl;
+
+    return 0;
+}
