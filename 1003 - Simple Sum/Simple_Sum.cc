374b10d95307b7b302ecc8fd38e1271f220c4b6b
kawsarkabir <devkawsarkabir@gmail.com>
2024-03-12 06:14:39 +0600

    1003 simple sum problem solved

---
+++ 1003 - Simple Sum/Simple_Sum.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int A, B;
+    std::cin >> A >> B; // Input the values of A and B
+
+    int SOMA = A + B; // Calculate the sum
+
+    // Output the result
+    std::cout << "SOMA = " << SOMA << std::endl;
+
+    return 0;
+}
---
+++ 1003 - Simple Sum/Simple_Sum.js
@@ -0,0 +1,5 @@
+const sum = (A, B) =>{
+    const SOMA = A + B;
+    return `SOMA = ${SOMA}`
+}
+console.log(sum(0, 0))
